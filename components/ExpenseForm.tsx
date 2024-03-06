d3d5d63392b5718f974365c09ec3126024087ae1
EnasKutbi <enaskutbi@gmail.com>
2024-03-06 06:29:27 +0300

    Basic Requirements

--- components/ExpenseForm.tsx
+++ components/ExpenseForm.tsx
@@ -1,23 +1,99 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { text } from "stream/consumers";
+
+type ExpenseType = {
+  id?: string;
+  source: string;
+  amount: number;
+  date: string;
+};
 
 const ExpenseForm = () => {
+
+  const [source, setSource] = useState<string>("");
+  const [amount, setAmount] = useState<number>(0);
+  const [date, setDate] = useState<string>("");
+
+  const [expenses, setExpenses] = useState<ExpenseType[]>([]);
+
+    const handleSourceChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setSource(value);
+    };
+
+    const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setAmount(Number(value));
+    };
+
+    const handleDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setDate(value);
+    };
+
+    const handleSubmit = (event: FormEvent) => {
+      event.preventDefault();
+      const expense = {
+        id: uuidv4(),
+        source: source,
+        amount: amount,
+        date: date,
+      };
+      setExpenses((prevExpenses) => {
+        return [...prevExpenses, expense];
+      });
+
+      setSource("");
+      setAmount(0);
+      setDate("");
+    };
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label htmlFor="source">Expense source</label>
-          <input type="text" name="source" id="source" required />
+          <input
+            type="text"
+            name="source"
+            id="source"
+            value={source}
+            onChange={handleSourceChange}
+            required
+          />
         </div>
         <div className="form-field">
           <label htmlFor="amount">Amount of Expense</label>
-          <input type="number" name="amount" id="amount" required />
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={amount}
+            onChange={handleAmountChange}
+            required
+          />
         </div>
         <div className="form-field">
           <label htmlFor="date">Date of Expense</label>
-          <input type="date" name="date" id="date" required />
+          <input
+            type="date"
+            name="date"
+            id="date"
+            value={date}
+            onChange={handleDateChange}
+            required
+          />
         </div>
         <button>Add Expense</button>
       </form>
+      <ul>{expenses.map((expense) => {
+        return (
+          <li key={expense.id}>
+            {expense.source}: {expense.amount} EUR on {expense.date}
+          </li>
+        );
+      })}</ul>
     </div>
   );
 };
--- components/TargetForSaving.tsx
+++ components/TargetForSaving.tsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 
 const TargetForSaving = () => {
+
+  const [target, setTarget] = useState<number>(0);
+  
+  const handleTargetChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setTarget(Number(value));
+  };
+
+const handleSubmit = (event: FormEvent) => {
+  event.preventDefault();
+  setTarget(target);
+  
+  setTarget(0);
+};
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label htmlFor="source">Set Target</label>
-          <input type="number" name="amount" id="amount" required />
-              </div>
-              <button>Reset</button>
-          </form>
-          <p>Target: 4000</p>
-          <p>Current Saving: 1000</p>
-          <p><progress max={4000} value={1000} /></p>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={target}
+            onChange={handleTargetChange}
+            required
+          />
+        </div>
+        <button>Reset</button>
+      </form>
+      <p>Target: {target}</p>
+      <p>Current Saving: 1000</p>
+      <p>
+        <progress max={4000} value={1000} />
+      </p>
     </div>
   );
 };
